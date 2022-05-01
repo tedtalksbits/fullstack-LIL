42df7480d0197c2ec852801a6b328e9592b75269
tedtalksbits <tedaneblake@gmail.com>
2022-05-01 09:59:59 -0400

    todo: webpack error

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const App = () => {
-  return <div>App</div>;
+const App = ({ message = "Hi there👋" }) => {
+  return <div>App{message}</div>;
 };
 
+App.prototype = {
+  message: PropTypes.string,
+};
 export default App;
